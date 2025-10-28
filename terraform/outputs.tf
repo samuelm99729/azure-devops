419851f1a954ddd8395ba85761c8ce244b388b12
SamuelM997 <msam99729@gmail.com>
2025-10-29 00:36:11 +0530

    Added CI/CD pipeline

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -1,31 +1,14 @@
-output "resource_group_name" {
-  value = azurerm_resource_group.rg.name
-}
-
 output "acr_login_server" {
-  value = azurerm_container_registry.acr.login_server
-}
-
-output "acr_admin_username" {
-  value     = azurerm_container_registry.acr.admin_username
-  sensitive = true
-}
-
-output "acr_admin_password" {
-  value     = azurerm_container_registry.acr.admin_password
-  sensitive = true
+  description = "ACR login server URL"
+  value       = azurerm_container_registry.acr.login_server
 }
 
 output "aks_cluster_name" {
-  value = azurerm_kubernetes_cluster.aks.name
+  description = "AKS cluster name"
+  value       = azurerm_kubernetes_cluster.aks.name
 }
 
-output "aks_kube_config" {
-  value     = azurerm_kubernetes_cluster.aks.kube_config_raw
-  sensitive = true
-}
-
-output "aks_host" {
-  value     = azurerm_kubernetes_cluster.aks.kube_config.0.host
-  sensitive = true
+output "resource_group_name" {
+  description = "Name of the resource group"
+  value       = azurerm_resource_group.rg.name
 }
